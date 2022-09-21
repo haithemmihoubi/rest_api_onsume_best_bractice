461fa9aca400b01dff53874eca24fe786fb6a858
Haithem Mihoubi <haithemmihoubi1234@gmail.com>
2022-09-21 10:08:41 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/rest_api_onsume_best_bractice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rest_api_onsume_best_bractice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/static/static_values.dart
@@ -0,0 +1,6 @@
+class StaticValues {
+  const StaticValues._();
+  static const apiUri = 'https://newsapi.org/v2/top-headlines';
+  static const apiKey = 'e4821d3ee9104b98baf8b0a7099934ef';
+  static const country = 'us';
+}
