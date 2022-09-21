620a0a5a7e5471b1d2fcf3c39c7c93ecf311da84
Haithem Mihoubi <haithemmihoubi1234@gmail.com>
2022-09-21 10:46:49 +0200

    generating the dart article model

--- lib/models/article_model.dart
+++ lib/models/article_model.dart
@@ -1,10 +1,37 @@
-import 'package:json_annotation/json_annotation.dart';
 
+import 'package:json_annotation/json_annotation.dart';
+part 'article_model.g.dart';
 @JsonSerializable()
 class Article {
+  @JsonKey(defaultValue: 'unKnown')
+  final String? author;
+  final String? title;
+  final String? description;
+  final String? url;
+  @JsonKey(name: 'urlToImage')
+  final String? imageUrl;
+  final String publishedAt;
+  final String? content;
+  final ArticleSource source;
 
+  Article(this.author, this.title, this.description, this.url, this.imageUrl,
+      this.publishedAt, this.content, this.source);
 
-
+  factory Article.fromJson(Map<String, dynamic> json) => _$ArticleFromJson(json);
+  Map<String, dynamic> toJson() => _$ArticleToJson(this);
+}
+//? serializing the sourcea in the article
+@JsonSerializable()
+class ArticleSource {
+  final String? id;
+  final String? name;
+  ArticleSource({
+    this.id,
+     this.name,
+  });
+factory ArticleSource.fromJson(Map<String, dynamic> json) =>
+      _$ArticleSourceFromJson(json);
 
 
 }
+
---
+++ lib/models/article_model.g.dart
@@ -0,0 +1,41 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'article_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Article _$ArticleFromJson(Map<String, dynamic> json) => Article(
+      json['author'] as String? ?? 'unKnown',
+      json['title'] as String?,
+      json['description'] as String?,
+      json['url'] as String?,
+      json['urlToImage'] as String?,
+      json['publishedAt'] as String,
+      json['content'] as String?,
+      ArticleSource.fromJson(json['source'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$ArticleToJson(Article instance) => <String, dynamic>{
+      'author': instance.author,
+      'title': instance.title,
+      'description': instance.description,
+      'url': instance.url,
+      'urlToImage': instance.imageUrl,
+      'publishedAt': instance.publishedAt,
+      'content': instance.content,
+      'source': instance.source,
+    };
+
+ArticleSource _$ArticleSourceFromJson(Map<String, dynamic> json) =>
+    ArticleSource(
+      id: json['id'] as String?,
+      name: json['name'] as String?,
+    );
+
+Map<String, dynamic> _$ArticleSourceToJson(ArticleSource instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+    };
