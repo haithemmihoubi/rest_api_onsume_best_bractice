34d5fef44d11d61dd6e3d8e80d8a6218d77c5ab6
Haithem Mihoubi <haithemmihoubi1234@gmail.com>
2022-09-27 14:16:26 +0200

    adding  personalized logger

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:logger/logger.dart';
 import 'package:rest_api_onsume_best_bractice/views/screens/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // You can optionally redirect all the logging messages from Get. If you want to use your own, favourite logging package, and want to capture the logs there:
+    void localLogWriter(String text, {bool isError = false}) {
+      Logger().i(text);
+
+    }
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
+      enableLog: true,
+      logWriterCallback: localLogWriter,
 
       title: 'Flutter Demo',
       theme: FlexThemeData.light(
@@ -46,9 +52,6 @@ class MyApp extends StatelessWidget {
         // To use the playground font, add GoogleFonts package and uncomment
         // fontFamily: GoogleFonts.notoSans().fontFamily,
       ),
-// If you do not have a themeMode switch, uncomment this line
-// to let the device system mode control the theme mode:
-// themeMode: ThemeMode.system,
 
       home:  HomePage(),
     );
--- lib/views/components/product_conmponent.dart
+++ lib/views/components/product_conmponent.dart
@@ -4,12 +4,13 @@ import 'package:rest_api_onsume_best_bractice/models/product.dart';
 class ProductComponent extends Card {
 ProductComponent({required Product product})
       : super(
-          child: Column(
-            children: [
-              Image.network(product.image),
-              Text(product.title),
-              Text(product.price.toString()),
-            ],
+          child: Card(
+            child: Column(
+              children: [
+                Text(product.title??""),
+                Text(product.price.toString()),
+              ],
+            ),
           ),
         );
 
